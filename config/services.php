ee24e90dc71ed1b1ff4b4e1ca6782f80e176a417
SufiWafry <sufisaw999@gmail.com>
2024-10-01 05:35:45 +0800

    1.10 testPayment

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+    'chip' => [
+    'api_key' => env('CHIP_API_KEY'),
+    'endpoint' => 'https://gate.chip-in.asia/api/v1/', // Ensure this is the actual API base URL
+],
 
 ];
